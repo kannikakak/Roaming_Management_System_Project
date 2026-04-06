7aa9e4c2b65037548dd762536ddd7efbb2773a93
kannikakak <kakkannika896@gmail.com>
2026-04-06 17:19:30 +0700

    fix: Remove unused Suspense import in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 import Login from "./pages/Login";
